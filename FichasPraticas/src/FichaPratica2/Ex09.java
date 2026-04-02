77e934f9e07950f24d0ee5b84d746a1040fe8df4
Paula <paulaguollo00@gmail.com>
2026-04-02 15:16:17 +0100

    ex10

---
+++ FichasPraticas/src/FichaPratica2/Ex09.java
@@ -0,0 +1,34 @@
+package FichaPratica2;
+
+import java.util.Scanner;
+
+public class Ex09 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("__________ F02 | EX09 ___________");
+        System.out.println("Wich is the min value?");
+
+        int value1, value2, value3;
+
+        System.out.print("Enter first value: ");
+        value1 = input.nextInt();
+
+        System.out.print("Enter second value: ");
+        value2 = input.nextInt();
+
+        System.out.print("Enter third value: ");
+        value3 = input.nextInt();
+
+        if(value1 < value2 && value1 < value3) {
+            System.out.println("This is the min value: " + value1 + " | This is the first value you entered");
+        }
+        if(value2 < value1 && value2 < value3) {
+            System.out.println("This is the min value: " + value2 + " | This is the second value you entered");
+        }
+        if(value3 < value1 && value3 < value2) {
+            System.out.println("This is the min value: " + value3 + " | This is the third value you entered");
+        }
+
+    }
+}
---
+++ FichasPraticas/src/FichaPratica2/Ex11.java
@@ -0,0 +1,14 @@
+package FichaPratica2;
+
+import java.util.Scanner;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("__________ F02 | EX11 ___________");
+        System.out.println();
+
+
+    }
+}
