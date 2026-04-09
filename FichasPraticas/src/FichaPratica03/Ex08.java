448d7bfc97e0b5f739525faefec7052b8183cf82
Paula <paulaguollo00@gmail.com>
2026-04-09 10:14:26 +0100

    ex08 PL03

--- FichasPraticas/src/FichaPratica03/Ex08.java
+++ FichasPraticas/src/FichaPratica03/Ex08.java
@@ -5,12 +5,27 @@ import java.util.Scanner;
 public class Ex08 {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
-        System.out.println("__________ F03 | EX07 ___________");
+        System.out.println("__________ F03 | EX08 ___________");
         System.out.println("A program that reads an integer and prints the 5 previous and the 5 following.");
 
-        int num;
+        int num, ant, suc;
 
         System.out.print("Enter a number: ");
         num = input.nextInt();
+
+     ant=num-5;
+     suc=num+5;
+
+    while(ant<num) { //o limite é o numero
+        System.out.println(ant);
+        ant++; //eu achei qual o primeiro antecessor e entao vou pra frente
+    }
+    num++; //preciso passar o numero para que ele nao apareça
+        System.out.println();
+
+        while(suc>=num) { //eu sei qual o sucessor final
+            System.out.println(num); //entao andamos o numero (num+1) até o sucessor
+            num++;
+        }
     }
 }
