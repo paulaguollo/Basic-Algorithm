732824d17a3e2263441d7f4a72955f8dabfddf2d
Paula Guollo <paulaguollo00@gmail.com>
2026-04-12 21:50:06 +0100

    ex03 extra ciclos

---
+++ FichasPraticas/src/FichaExtraCiclos/Ex03.java
@@ -0,0 +1,17 @@
+package FichaExtraCiclos;
+
+public class Ex03 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println("Tabuada do "+i+":");
+
+            for (int j = 1; j <= 10; j++) {
+                int resultado = i * j;
+                System.out.println(i+" x "+j+" = " + resultado);
+            }
+            System.out.println();
+        }
+
+    }
+}
